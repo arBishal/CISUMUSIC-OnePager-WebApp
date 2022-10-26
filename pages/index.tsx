f823890140c3820f169267cfa692599862dcd7e3
arBishal <arbishal.319@gmail.com>
2022-10-27 04:43:38 +0600

    button added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image'
 import logo from '../public/images/logo.png'
 import style from './index.module.css'
 import Upload from '../src/custom-components/upload'
+import { Button } from 'antd'
 
 const Home: NextPage = () => {
   return (
-    <div className={style.full}>
+    <div>
       <Head>
         <title>Cisumusic</title>
         <link rel="icon" href="/images/hlogo.png" />
@@ -22,8 +23,11 @@ const Home: NextPage = () => {
           width="336.6vh"
         />
         </div>
-        
+        <p>
+          Upload a piece of music to find out the genre. Enjoy!
+        </p>
         <Upload/>
+        <Button type="primary" style={{margin:"20px"}}>Find the genre!</Button>
       </div>
 
       <footer className={style.footer}>
